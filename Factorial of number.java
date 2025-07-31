f4244ea0cafabe26c5ce5b750c853c2be9da4613
Sakshi43-dot <kadamsakshi43@gmail.com>
2025-08-01 00:16:01 +0530

    Add files via upload

---
+++ Factorial of number.java
@@ -0,0 +1,13 @@
+public class Main 
+{
+    public static void main(String[]args)
+    {
+        int num=6;
+        int fact=1;
+        for(int i=num;i>=1;i--)
+        {
+            fact = fact*i;
+        }
+        System.out.println("Factorial:" + fact);
+    }
+}
